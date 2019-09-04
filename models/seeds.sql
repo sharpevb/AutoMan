bc65e3e05b87a5b7d6ab88323ef03ff9d5fc53d7
Deb <49885983+dawarrick@users.noreply.github.com>
2019-09-03 20:55:34 -0400

    add seeds

--- models/seeds.sql
+++ models/seeds.sql
@@ -6,3 +6,10 @@ values (2010, "Toyota","Rav-4",80000,"Silver",sysdate(),sysdate());
 
 insert into cars (year, make, model, mileage, color, createdat,updatedat)
 values (2013, "Subaru","Outback",94000,"Green", sysdate(),sysdate());
+
+insert into customers (name, address, email, phone, createdat,updatedat)
+values ("John Smith", "123 Lucy Lane, Charleston, WV","jsmith@gmail.com","917-555-1212", sysdate(),sysdate());
+
+insert into customers (name, address, email, phone, createdat,updatedat)
+values ("Jane Doe", "123 Sesame Street, Columbus, OH","janedoe@yahoo.com","712-555-1212", sysdate(),sysdate());
+
