384c845bd7c12bab5fe142d5620ca9add8c87876
sharpevb <victoriabsharpe@gmail.com>
2019-09-05 21:41:59 -0400

    deleted placeholder text from modals

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,17 +1,17 @@
-use car;
+use fth2zolugpxwyrzu;
 
-insert into cars (year, make, model, mileage, color, createdat,updatedat)
+insert into Cars (year, make, model, mileage, color, createdat,updatedat)
 values (2001, "Toyota","Camry",125000,"Beige", sysdate(),sysdate());
 
-insert into cars (year, make, model, mileage, color, createdat,updatedat)
+insert into Cars (year, make, model, mileage, color, createdat,updatedat)
 values (2010, "Toyota","Rav-4",80000,"Silver",sysdate(),sysdate());
 
-insert into cars (year, make, model, mileage, color, createdat,updatedat)
+insert into Cars (year, make, model, mileage, color, createdat,updatedat)
 values (2013, "Subaru","Outback",94000,"Green", sysdate(),sysdate());
 
-insert into customers (name, address, email, phone, createdat,updatedat)
+insert into Customers (name, address, email, phone, createdat,updatedat)
 values ("John Smith", "123 Lucy Lane, Charleston, WV","jsmith@gmail.com","917-555-1212", sysdate(),sysdate());
 
-insert into customers (name, address, email, phone, createdat,updatedat)
+insert into Customers (name, address, email, phone, createdat,updatedat)
 values ("Jane Doe", "123 Sesame Street, Columbus, OH","janedoe@yahoo.com","712-555-1212", sysdate(),sysdate());
 
